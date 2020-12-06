fa23aab501f167bf3b92eb9c4638e0d9e57303fe
Emanuel Di Cristofaro <emanuelesposito3@gmail.com>
2020-12-05 23:20:03 -0400

    CRUDS - Actualización #3
    -Arreglado el Dto de Marca.
    -Se añadió la API de Marca

--- MercadeoUCAB-backend/dtos/src/main/java/ucab/dsw/dtos/MarcaDto.java
+++ MercadeoUCAB-backend/dtos/src/main/java/ucab/dsw/dtos/MarcaDto.java
@@ -3,14 +3,22 @@ package ucab.dsw.dtos;
 public class MarcaDto extends DtoBase {
     
     private String _nombre;
+    private String _descripcion;
 
     public String getNombre() {
         return _nombre;
     }
 
-
     public void setNombre(String _nombre) {
         this._nombre = _nombre;
+    }
+
+    public String get_descripcion() {
+        return _descripcion;
+    }
+
+    public void set_descripcion(String _descripcion) {
+        this._descripcion = _descripcion;
     }
 
     public MarcaDto (long id) throws Exception{
---
+++ MercadeoUCAB-backend/servicio/src/test/java/SolicitudEstudioAPI_Test.java
@@ -0,0 +1,14 @@
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsNot;
+import org.junit.*;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class SolicitudEstudioAPI_Test {
+
+    //Esta prueba nos permite insertar una solicitud
+}
