03bb61e7341a514a7fb68e449686d278e73a59ae
GreggSpinetti <50221976+GreggSpinetti@users.noreply.github.com>
2020-12-07 20:01:34 -0400

    API de la tabla Rol creada.

---
+++ MercadeoUCAB-backend/servicio/src/main/java/ucab/dsw/servicio/RolAPI.java
@@ -0,0 +1,83 @@
+package ucab.dsw.servicio;
+
+import ucab.dsw.accesodatos.DaoRol;
+import ucab.dsw.dtos.RolDto;
+import ucab.dsw.entidades.Rol;
+
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path( "/rol" )
+@Produces( MediaType.APPLICATION_JSON )
+@Consumes( MediaType.APPLICATION_JSON )
+public class RolAPI {
+    @GET
+    @Path("/allRoles")
+    public List<Rol> listarRoles(){
+
+        DaoRol daoRol = new DaoRol();
+        return daoRol.findAll(Rol.class);
+    }
+
+    @GET
+    @Path("/consultarRol/{id}")
+    public Rol consultarRol(@PathParam("id") long id){
+        DaoRol daoRol = new DaoRol();
+        return daoRol.find(id, Rol.class);
+    }
+
+    @POST
+    @Path("/addRol")
+    public Rol addRol(RolDto rolDto){
+
+        DaoRol daoRol = new DaoRol();
+        Rol rol = new Rol();
+
+        rol.setNombre(rolDto.get_nombre());
+        rol.set_estatus("Activo");
+
+        return rol;
+    }
+
+    @PUT
+    @Path("/updateRol/{id}")
+    public Response updateRol(@PathParam("id") long id, RolDto rolDto){
+
+        DaoRol daoRol = new DaoRol();
+        Rol rol_modificar = daoRol.find(id, Rol.class);
+
+        if(rol_modificar != null){
+
+            rol_modificar.setNombre(rolDto.get_nombre());
+            rolDto.set_estatus(rolDto.get_estatus());
+            return Response.ok().entity(rol_modificar).build();
+
+        } else {
+
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
+    @DELETE
+    @Path("/deleteRol/{id}")
+    public Response deleteRol(@PathParam("id") long id){
+
+        DaoRol daoRol = new DaoRol();
+        Rol rol_eliminar = daoRol.find(id, Rol.class);
+
+        if(rol_eliminar != null){
+
+            daoRol.delete(rol_eliminar);
+            return Response.ok().entity(rol_eliminar).build();
+        } else {
+
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+    }
+
+
+}
