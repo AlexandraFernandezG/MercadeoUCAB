f1f8865ca283470ebfbc62d13c56afe36d07230c
GreggSpinetti <50221976+GreggSpinetti@users.noreply.github.com>
2020-12-17 01:02:18 -0400

    Finalizado el CRUD usuario.

--- MercadeoUCAB-backend/servicio/src/main/java/ucab/dsw/servicio/UsuarioAPI.java
+++ MercadeoUCAB-backend/servicio/src/main/java/ucab/dsw/servicio/UsuarioAPI.java
@@ -1,5 +1,6 @@
 package ucab.dsw.servicio;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import ucab.dsw.accesodatos.DaoRol;
 import ucab.dsw.accesodatos.DaoUsuario;
 import ucab.dsw.directorioactivo.DirectorioActivo;
@@ -8,6 +9,9 @@ import ucab.dsw.dtos.UsuarioDto;
 import ucab.dsw.entidades.Rol;
 import ucab.dsw.entidades.Usuario;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.mail.MessagingException;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -223,5 +227,44 @@ public class UsuarioAPI extends AplicacionBase {
 
     }
 
+    //Recuperar clave de usuario por correo
+    @POST
+    @Path("/recuperarClave")
+    @Produces( MediaType.APPLICATION_JSON )
+    @Consumes( MediaType.APPLICATION_JSON )
+    public JsonObject recuperarClaveUsuario(UsuarioDto usuarioDto) throws MessagingException {
 
+        Boolean registrado;
+        JsonObject respuesta = null;
+        String contenido;
+        DirectorioActivo directorioActivo = new DirectorioActivo();
+
+        try {
+
+            DirectorioActivo ldap = new DirectorioActivo();
+            //String correoElectronico = ldap.getCorreo();
+
+            //Este vendría siendo el correo que se optiene del get
+            String correoElectronico = "emanuelesposito3@gmail.com";
+
+            String randomClave = RandomStringUtils.randomAlphanumeric(10);
+
+            //UsuarioDto usuario = new UsuarioDto();
+            //usuario.setNombreUsuario(usuarioDto.getNombreUsuario());
+            //usuario.setContrasena(randomClave);
+            //directorioActivo.changePassword(usuario);
+
+            contenido = "Estimado " + usuarioDto.getNombreUsuario() + " su contraseña ha sido actualizada. Su nueva contraseña es: "+randomClave;
+            ucab.dsw.servicio.EmailUtility.enviarCorreoElectronico(correoElectronico, "Recuperacion de clave", contenido);
+            respuesta = Json.createObjectBuilder().add("estatus", "Correo enviado correctamente").build();
+
+        } catch (Exception ex){
+
+            String mensaje = ex.getMessage();
+            System.out.print(mensaje);
+        }
+
+        return respuesta;
+
+    }
 }
