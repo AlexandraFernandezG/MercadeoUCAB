19e3975e3839d37c32177a73bb878f8111fe0b0a
Emanuel Di Cristofaro <emanuelesposito3@gmail.com>
2021-01-04 03:12:54 -0400

    SERVICE - Actualización #17.1
    -Terminado la sugerencia Solicitud/Estudio
    
    -Añadido el método para insertar un nuevo estudio con preguntas (Partiendo del contenido de un estudio recomendado, la solicitud y el usuario quien la crea). Esta casi listo, falta un detallito.
    
    -Agregue el método y prueba Unitaria de Valentina.

--- MercadeoUCAB-backend/servicio/src/main/java/ucab/dsw/servicio/ReportesService.java
+++ MercadeoUCAB-backend/servicio/src/main/java/ucab/dsw/servicio/ReportesService.java
@@ -26,6 +26,12 @@ public class ReportesService extends AplicacionBase {
     @Produces( MediaType.APPLICATION_JSON )
     public List<RespuestasAbiertasResponse> listarRespuestasAbiertas(@PathParam("id") long id) throws NullPointerException{
 
+        /**
+         * Este método permite obtener las respuestas de las preguntas abiertas
+         *
+         * NOTA: Hay que realizar.
+         */
+
         try {
 
             return null;
@@ -39,59 +45,6 @@ public class ReportesService extends AplicacionBase {
         }
     }
 
-
-    /*
-    @GET
-    @Path("/respuestasPreguntaAbierta/{id}")
-    @Produces( MediaType.APPLICATION_JSON )
-    public List<RespuestasAbiertasResponse> listarRespuestasAbiertas(@PathParam("id") long id) throws NullPointerException {
-
-        String SQL = null;
-
-        DaoPreguntaEncuesta daoPreguntaEncuesta = new DaoPreguntaEncuesta();
-        PreguntaEncuesta preguntaEncuesta = daoPreguntaEncuesta.find(id, PreguntaEncuesta.class);
-
-        try {
-
-            String tipoPregunta = preguntaEncuesta.get_tipoPregunta();
-
-            if(tipoPregunta.equals("Abierta")){
-
-                EntityManagerFactory factory = Persistence.createEntityManagerFactory("mercadeoUcabPU");
-                EntityManager entitymanager = factory.createEntityManager();
-
-                SQL = "SELECT re._id as id, re._respuestaAbierta as respuestaAbierta FROM Respuesta as re, PreguntaEstudio as pes, PreguntaEncuesta as pe " +
-                        "WHERE pe._id = pes._preguntaEncuesta._id and pes._id = re._preguntaEstudio._id and pe._tipoPregunta = :tipoPregunta and pe._id = :id";
-
-                Query query = entitymanager.createQuery(SQL);
-                query.setParameter("id", id);
-                query.setParameter("tipoPregunta", tipoPregunta);
-
-                List<Object[]> listaRespuestas = query.getResultList();
-
-                List<RespuestasAbiertasResponse> listaRespuestasAbiertas = new ArrayList<>(listaRespuestas.size());
-
-                for (Object[] res: listaRespuestas){
-
-                    listaRespuestasAbiertas.add(new RespuestasAbiertasResponse((long)res[0], (String)res[1]));
-                }
-
-                return listaRespuestasAbiertas;
-
-            } else {
-
-                List<RespuestasAbiertasResponse> vacio = new ArrayList<>();
-                return vacio;
-            }
-
-        } catch (NullPointerException ex) {
-
-            String mensaje = ex.getMessage();
-            System.out.print(mensaje);
-            return null;
-        }
-    }*/
-
     @GET
     @Path("/porcentajeVF/{id}")
     @Produces( MediaType.APPLICATION_JSON )
@@ -101,7 +54,7 @@ public class ReportesService extends AplicacionBase {
          * Este método permite obtener un poncentaje de respuestas de verdadero y
          * falso para un estudio.
          *
-         * NOTA: Esta casi listo, pero falta mandar como json.
+         * NOTA: Hay que acomodarlo separando ambos querys en dos metodos distintos
          */
 
         try {
