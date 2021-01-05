a95f55deefcb08b213968fc2682273128534746e
Emanuel Di Cristofaro <emanuelesposito3@gmail.com>
2021-01-05 05:34:52 -0400

    SERVICE - Actualización #20.2
    -Arreglado el metodo cliente/Estudio
    
    -Un poco mejorado el metodo Solicitud/Estudio

--- MercadeoUCAB-backend/servicio/src/main/java/ucab/dsw/servicio/SuggestionsService.java
+++ MercadeoUCAB-backend/servicio/src/main/java/ucab/dsw/servicio/SuggestionsService.java
@@ -100,9 +100,9 @@ public class SuggestionsService extends AplicacionBase {
                 EntityManagerFactory factory = Persistence.createEntityManagerFactory("mercadeoUcabPU");
                 EntityManager entitymanager = factory.createEntityManager();
 
-                SQL = "SELECT e._id as idEstudio, e._nombre as nombre, e._tipoInstrumento as tipoInstrumento, e._fechaInicio as fechaInicio, e._fechaFin as fechaFin, e._estatus as estatus " +
-                        "FROM Estudio as e, Usuario as u, Informacion as inf " +
-                        "WHERE e._usuario._id = u._id and u._id = inf._usuario._id and " +
+                SQL = "SELECT DISTINCT e._id as idEstudio, e._nombre as nombre, e._tipoInstrumento as tipoInstrumento, e._fechaInicio as fechaInicio, e._fechaFin as fechaFin, e._estatus as estatus " +
+                        "FROM Estudio as e, Informacion as inf, SolicitudEstudio as se, MedioComunicacion as me " +
+                        "WHERE e._solicitudEstudio._id = se._id and se._id = me._solicitudEstudio._id and me._informacion._id = inf._id and " +
                         "inf._genero = :genero and " +
                         "(inf._estadoCivil = :estadoCivil or inf._estadoCivil is null) and " +
                         "(inf._cantidadPersonas = :cantidadPersonas or inf._cantidadPersonas is null)";
@@ -323,8 +323,9 @@ public class SuggestionsService extends AplicacionBase {
             EntityManagerFactory factory = Persistence.createEntityManagerFactory("mercadeoUcabPU");
             EntityManager entitymanager = factory.createEntityManager();
 
-            SQL = "SELECT e._id as idEstudio, e._nombre as nombre, e._tipoInstrumento as tipoInstrumento, e._fechaInicio as fechaInicio, e._fechaFin as fechaFin, e._estatus as estatus " +
-                    "FROM Estudio as e, Usuario as u WHERE e._usuario._id = u._id and u._id = :id";
+            SQL = "SELECT DISTINCT e._id as idEstudio, e._nombre as nombre, e._tipoInstrumento as tipoInstrumento, e._fechaInicio as fechaInicio, e._fechaFin as fechaFin, e._estatus as estatus " +
+                    "FROM Estudio as e, Usuario as u, SolicitudEstudio as se WHERE e._solicitudEstudio._id = se._id and " +
+                    "se._usuario._id = u._id and u._id = :id";
 
             Query query = entitymanager.createQuery(SQL);
             query.setParameter("id", id);
