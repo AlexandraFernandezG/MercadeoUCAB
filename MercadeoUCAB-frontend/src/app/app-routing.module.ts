4ad53f8ed6060854f41f3dd98ab521488f5b6c92
Valentina Figueroa <valenfigue14@gmail.com>
2021-01-20 17:38:44 -0400

    Mejora diseño tablas - ADMIN #1

--- MercadeoUCAB-frontend/src/app/app-routing.module.ts
+++ MercadeoUCAB-frontend/src/app/app-routing.module.ts
@@ -59,7 +59,7 @@ const routes: Routes = [
     ]
   },
 
-  { path: 'cliente', component: ClienteComponent, 
+  { path: 'cliente', component: ClienteComponent,
     children: [
       { path: 'estudios', component: EstudiosClienteComponent },
       { path: 'solicitar_estudio', component: SolicitudEstudioComponent },
@@ -68,7 +68,7 @@ const routes: Routes = [
       { path: '', pathMatch: 'prefix', redirectTo: 'estudios'},
     ]
   },
-  
+
   { path: 'encuestado', component: EncuestadoComponent,
     children: [
       { path: 'estudios', component: EstudiosEncuestadoComponent },
@@ -77,7 +77,7 @@ const routes: Routes = [
       { path: '', pathMatch: 'prefix', redirectTo: 'estudios'},
     ]
   },
-  
+
   { path: 'analista', component: AnalistaComponent,
     children: [
       { path: 'solicitudes', component: EstudiosAnalistaComponent },
