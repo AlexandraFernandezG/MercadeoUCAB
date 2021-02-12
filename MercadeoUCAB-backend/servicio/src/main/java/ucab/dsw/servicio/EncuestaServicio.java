0d5e85b26aa3f0f19a81f73189be8a113dbf1319
GreggSpinetti <50221976+GreggSpinetti@users.noreply.github.com>
2021-02-12 15:53:55 -0400

    Añadidos el loggers.

--- MercadeoUCAB-backend/servicio/src/main/java/ucab/dsw/servicio/EncuestaServicio.java
+++ MercadeoUCAB-backend/servicio/src/main/java/ucab/dsw/servicio/EncuestaServicio.java
@@ -15,11 +15,15 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path( "/encuesta" )
 @Produces( MediaType.APPLICATION_JSON )
 @Consumes( MediaType.APPLICATION_JSON )
 public class EncuestaServicio {
+
+    private static Logger logger = LoggerFactory.getLogger(EncuestaServicio.class);
 
     /**
      * Este método permite obtener las preguntas de una encuesta
@@ -32,6 +36,7 @@ public class EncuestaServicio {
     @Consumes( MediaType.APPLICATION_JSON )
     public Response obtenerPreguntaEncuesta (@PathParam("id") long id,@PathParam("idUsuario") long idUsuario) {
 
+        logger.debug("Ingresando al método que muestra las preguntas de una encuesta");
         DaoPreguntaEncuesta daoPreguntaEncuesta = new DaoPreguntaEncuesta();
         JsonObject dataObject;
 
@@ -46,6 +51,7 @@ public class EncuestaServicio {
                 ResponseListUpdate.add(new EncuestaResponse((long)r[0], (String)r[1], (String)r[2], (long)r[3]));
             }
 
+            logger.debug("Saliendo del método que muestra las preguntas de una encuesta");
             return Response.status(Response.Status.OK).entity(ResponseListUpdate).build();
 
         } catch (Exception ex) {
@@ -55,6 +61,8 @@ public class EncuestaServicio {
                     .add("excepcion", ex.getMessage())
                     .add("codigo", 400).build();
 
+
+            logger.error("Código de error: " + 400 +  ", Mensaje de error: " + ex.getMessage());
             return Response.status(Response.Status.BAD_REQUEST).entity(dataObject).build();
         }
 
@@ -71,6 +79,7 @@ public class EncuestaServicio {
     @Consumes( MediaType.APPLICATION_JSON )
     public Response obtenerRespuestaEncuesta(@PathParam("id") long id) {
 
+        logger.debug("Ingresando al método que muestra las respuestas de las preguntas de una encuesta");
         DaoRespuestaPregunta daoRespuestaPregunta = new DaoRespuestaPregunta();
         JsonObject dataObject;
 
@@ -85,6 +94,7 @@ public class EncuestaServicio {
                 ResponseListUpdate.add(new RespuestaPreguntaResponse((Long)r[0], (String)r[1]));
             }
 
+            logger.debug("Saliendo del método que muestra las respuestas de las preguntas de una encuesta");
             return Response.status(Response.Status.OK).entity(ResponseListUpdate).build();
 
         } catch (Exception ex) {
@@ -94,6 +104,7 @@ public class EncuestaServicio {
                     .add("excepcion", ex.getMessage())
                     .add("codigo", 400).build();
 
+            logger.error("Código de error: " + 400 +  ", Mensaje de error: " + ex.getMessage());
             return Response.status(Response.Status.BAD_REQUEST).entity(dataObject).build();
         }
     }
@@ -109,6 +120,7 @@ public class EncuestaServicio {
     @Consumes( MediaType.APPLICATION_JSON )
     public Response getAllByRespuesta(@PathParam("id") long id) {
 
+        logger.debug("Ingresando al método que muestra los encuestados de un estudio");
         DaoUsuario daoUsuario = new DaoUsuario();
         JsonObject dataObject;
 
@@ -122,6 +134,7 @@ public class EncuestaServicio {
                 estudioUsuarioResponseListUpdate.add(new EstudioEncuestadoResponse((Long)r[1], (String)r[2], (String)r[3]));
             }
 
+            logger.debug("Saliendo del método que muestra los encuestados de un estudio");
             return Response.status(Response.Status.OK).entity(estudioUsuarioResponseListUpdate).build();
 
         } catch (Exception ex) {
@@ -131,6 +144,7 @@ public class EncuestaServicio {
                     .add("excepcion", ex.getMessage())
                     .add("codigo", 400).build();
 
+            logger.error("Código de error: " + 400 +  ", Mensaje de error: " + ex.getMessage());
             return Response.status(Response.Status.BAD_REQUEST).entity(dataObject).build();
         }
 
@@ -147,6 +161,8 @@ public class EncuestaServicio {
     @Consumes( MediaType.APPLICATION_JSON )
     public Response addRespuesta(List<RespuestaDto> respuestas) {
 
+
+        logger.debug("Ingresando al método que permite responder una encuesta");
         JsonObject dataObject;
         RespuestaDto resultado = new RespuestaDto();
 
@@ -179,6 +195,7 @@ public class EncuestaServicio {
 
             }
 
+            logger.debug("Saliendo del método que permite responder una encuesta");
             return Response.status(Response.Status.OK).entity(resultado).build();
 
         } catch (PruebaExcepcion ex) {
@@ -186,8 +203,9 @@ public class EncuestaServicio {
             dataObject = Json.createObjectBuilder()
                     .add("estado", "Error")
                     .add("excepcion", ex.getMessage())
-                    .add("codigo", 400).build();
+                    .add("codigo", 402).build();
 
+            logger.error("Código de error: " + 402 +  ", Mensaje de error: " + ex.getMessage());
             return Response.status(Response.Status.BAD_REQUEST).entity(dataObject).build();
 
         } catch (NullPointerException ex) {
@@ -197,6 +215,7 @@ public class EncuestaServicio {
                     .add("excepcion", ex.getMessage())
                     .add("codigo", 401).build();
 
+            logger.error("Código de error: " + 401 +  ", Mensaje de error: " + ex.getMessage());
             return Response.status(Response.Status.BAD_REQUEST).entity(dataObject).build();
 
         }
@@ -210,6 +229,7 @@ public class EncuestaServicio {
      @Path("/validarEstatusEncuesta/{idE}/{idU}")
      public Response validarEstatusEncuestas(@PathParam("idE") long idE, @PathParam("idU") long idU) {
 
+         logger.debug("Ingresando al método que valida el estatus de una encuesta");
          JsonObject dataObject;
          Object validar;
 
@@ -244,6 +264,7 @@ public class EncuestaServicio {
 
                 }
 
+             logger.debug("Saliendo del método que valida el estatus de una encuesta");
              return Response.status(Response.Status.OK).entity(validar).build();
 
          } catch (Exception ex) {
@@ -253,6 +274,7 @@ public class EncuestaServicio {
                      .add("excepcion", ex.getMessage())
                      .add("codigo", 400).build();
 
+             logger.error("Código de error: " + 400 +  ", Mensaje de error: " + ex.getMessage());
              return Response.status(Response.Status.BAD_REQUEST).entity(dataObject).build();
 
          }
